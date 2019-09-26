c1c52a24eb6e8de3918c28a203da32724eabfcce
Armand Syahtama <armand.syahtama@gmail.com>
2019-09-26 19:13:43 -0400

    added benign dataset

--- Scripts/phishtank.py
+++ Scripts/phishtank.py
@@ -6,7 +6,7 @@ phish_urls = []
 
 with open(filepath) as json_file:
     data = json.load(json_file)
-    for phish_url in data[:500]:
+    for phish_url in data:
         phish_urls.append(phish_url['url'])
     
 with open('../Datasets/retrieved_phish_urls.txt', 'w', encoding="utf-8") as txt_file:
