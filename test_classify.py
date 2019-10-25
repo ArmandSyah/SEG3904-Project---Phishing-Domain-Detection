c2b01890edd94ad274853ce5e6c61487fd3c1922
Armand Syahtama <armand.syahtama@gmail.com>
2019-10-25 10:34:57 -0400

    small changes

--- test_classify.py
+++ test_classify.py
@@ -10,22 +10,28 @@ with open('BuiltModels\\naive_bayes.pkl', 'rb') as nb_pickle:
     nb = pickle.load(nb_pickle)
 
 # Legit
+print('Decision Tree Results\n')
 dt.predict_url('https://www.reddit.com/r/Games/comments/dmgu22/sources_the_last_of_us_2_delayed_to_spring/')
 dt.predict_url('https://stackoverflow.com/questions/16729574/how-to-get-a-value-from-a-cell-of-a-dataframe')
 # Not Legit
 dt.predict_url('https://qiita.com/UEFA-Europa-LeaguS/items/5c4ed0ed45b104eb877a')
 dt.predict_url('https://www439.promocao-master.club/pt-psl6208734115db/smart-tv-led-40-samsung-ultra-hd-4k-40nu7100-com-conversor-digital-3-hdmi-2-usb-wi-fi-hdr-premium-smart-tizen/133756442/pr')
+dt.predict_test_set()
 
+print('\nRandom Forest Results\n')
 # Legit
 rf.predict_url('https://www.reddit.com/r/Games/comments/dmgu22/sources_the_last_of_us_2_delayed_to_spring/')
 rf.predict_url('https://stackoverflow.com/questions/16729574/how-to-get-a-value-from-a-cell-of-a-dataframe')
 # Not Legit
 rf.predict_url('https://qiita.com/UEFA-Europa-LeaguS/items/5c4ed0ed45b104eb877a')
 rf.predict_url('https://www439.promocao-master.club/pt-psl6208734115db/smart-tv-led-40-samsung-ultra-hd-4k-40nu7100-com-conversor-digital-3-hdmi-2-usb-wi-fi-hdr-premium-smart-tizen/133756442/pr')
+rf.predict_test_set()
 
+print('\nNaive Bayes Results\n')
 # Legit
 nb.predict_url('https://www.reddit.com/r/Games/comments/dmgu22/sources_the_last_of_us_2_delayed_to_spring/')
 nb.predict_url('https://stackoverflow.com/questions/16729574/how-to-get-a-value-from-a-cell-of-a-dataframe')
 # Not Legit
 nb.predict_url('https://qiita.com/UEFA-Europa-LeaguS/items/5c4ed0ed45b104eb877a')
 nb.predict_url('https://www439.promocao-master.club/pt-psl6208734115db/smart-tv-led-40-samsung-ultra-hd-4k-40nu7100-com-conversor-digital-3-hdmi-2-usb-wi-fi-hdr-premium-smart-tizen/133756442/pr')
+nb.predict_test_set()
